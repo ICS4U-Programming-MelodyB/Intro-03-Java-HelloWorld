1002a1514f350887071fdee4d5bbbac6808eca9d
Melody Berhane <melody.berhane@stu.ocsb.ca>
2023-02-08 13:54:54 +0000

    Commit message

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,10 @@
-//Prints Hello World in terminal
+^$
+/*
+Prints Hello World in terminal
+@author  Melody Berhane
+@version 1.0
+@since   2023-1-07
+ */
 class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!");
